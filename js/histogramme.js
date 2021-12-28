d4da8c58a070fc8fc32f08f6bc63ede100951c27
Aurélien <aurelien.bresson@etu.univ-lyon1.fr>
2021-12-28 18:57:05 +0100

    reorganize files to separate vizualisations

--- js/histogramme.js
+++ js/histogramme.js
@@ -12,7 +12,7 @@ document.addEventListener ("DOMContentLoaded", () => {
         .range([height, 0])
 
 // append the svg object to the body of the page
-    const svg = d3.select("#my_dataviz")
+    const svg = d3.select("#histogram")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
